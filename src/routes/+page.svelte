c19f63f7e9572eb74becb740ff9a434297579b39
Sina-Hosseini-GST <sina.hosseini.gst@gmail.com>
2024-11-16 12:27:51 +0330

    Change UI, and fix default options for both social inputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,10 +17,12 @@
 	let socialMedia = $state('');
 	let userName = $state('');
 
+	let defaultSocialMedia = 'Select Account If Added';
+	
 	let inputText = $state('');
-	let inputSocialMedia = $state('');
+	let inputSocialMedia = $state(defaultSocialMedia);
 	let outputText = $state('');
-	let outputSocialMedia = $state('');
+	let outputSocialMedia = $state(defaultSocialMedia);
 
 	$effect(() => {
 		const localStorageInformations = JSON.parse(localStorage.getItem('informations'));
@@ -29,7 +31,7 @@
 		}
 	});
 
-	let entryIndex = 0;
+	let entryIndex = - 1;
 
 	const addEntry = (event) => {
 		event.preventDefault();
@@ -49,7 +51,7 @@
 					for (let j = 0; j < informations[i].accounts.length; j++) {
 						if (informations[i].accounts[j].socialMedia === socialMedia) {
 							innerFlag = false;
-							alert(`${socialMedia} added for ${name}`);
+							alert(`${name} has ${socialMedia} account already!`);
 						}
 					}
 
@@ -66,7 +68,6 @@
 
 			if (flag) {
 				informations.push({
-					id: informations.length ? informations[informations.length - 1].id + 1 : 1,
 					name,
 					accounts: [
 						{
@@ -77,7 +78,7 @@
 				});
 			}
 
-			if (entryIndex === 1) {
+			if (entryIndex === 0) {
 				inputSocialMedia = socialMedia;
 				outputSocialMedia = socialMedia;
 			}
@@ -126,7 +127,7 @@
 											// same entry
 											if (information.name === outerInformation.name && account.socialMedia === outputSocialMedia) {
 												innerFlag = false;
-												outputText += `<span class='text-[red]'>${account.userName}</span>${space}`;
+												outputText += `<span class='text-[red] font-bold'>${account.userName}</span>${space}`;
 												break loop;
 											}
 										}
@@ -178,28 +179,28 @@
 	<main class="flex flex-1 overflow-hidden xl:gap-3">
 		<section class="flex flex-col w-1/4 xl:gap-3">
 			<div class="flex xl:gap-3">
-				<button class="w-1/2 tracking-wider text-white bg-green-600 transition-colors duration-[250ms] hover:bg-green-500 text-center leading-[3rem] rounded" onclick={ localStorageHandler }>
+				<button class="w-1/2 tracking-wider text-white bg-emerald-400 transition-colors duration-[250ms] hover:bg-emerald-500 text-center leading-[3rem] rounded" onclick={ localStorageHandler }>
 					Export
 				</button>
-				<button class="w-1/2 tracking-wider text-white bg-green-600 transition-colors duration-[250ms] hover:bg-green-500 text-center leading-[3rem] rounded">
+				<button class="w-1/2 tracking-wider text-white bg-emerald-400 transition-colors duration-[250ms] hover:bg-emerald-500 text-center leading-[3rem] rounded">
 					Import
 				</button>
 			</div>
-			<h1 class="tracking-wider text-white bg-lime-500 text-center leading-[3rem] border-b border-white rounded">
+			<h1 class="tracking-wider text-white bg-green-500 text-center leading-[3rem] rounded">
 				Accounts Added
 			</h1>
 			<div class="flex flex-col overflow-auto xl:gap-3">
 				{#each informations as information, i}
 					<div class="tracking-widest text-white">
 						{#if information.accounts.length}
-							<input placeholder="Give the account a name (e.g., Sina)" class="sticky top-0 z-10 w-full border-b border-white bg-lime-400 xl:leading-10 xl:px-3 focus:outline-none placeholder:text-gray-100" bind:value={information.name}>
+							<input placeholder="Give the account a name (e.g., Sina)" class="sticky top-0 z-10 w-full border-b border-white bg-lime-500 xl:leading-10 xl:px-3 focus:outline-none placeholder:text-gray-100" bind:value={information.name}>
 						{/if}
 						<ul class="text-xs">
 							{#each information.accounts as account, j}
 								<li class="flex border-b border-white xl:leading-8">
 									<div class="flex flex-1">
-										<input placeholder="Social media (e.g., Instagram)" class="w-1/2 bg-blue-400 border-r border-white xl:px-3 focus:outline-none placeholder:text-gray-100" bind:value={account.socialMedia}>
-										<input placeholder="@username (e.g., @sinaGST)" class="w-1/2 bg-blue-400 border-r border-white xl:px-3 focus:outline-none placeholder:text-gray-100" bind:value={account.userName}>
+										<input placeholder="Social media (e.g., Instagram)" class="w-1/2 bg-lime-400 border-r border-white xl:px-3 focus:outline-none placeholder:text-gray-100" bind:value={account.socialMedia}>
+										<input placeholder="@username (e.g., @sinaGST)" class="w-1/2 bg-lime-400 border-r border-white xl:px-3 focus:outline-none placeholder:text-gray-100" bind:value={account.userName}>
 									</div>
 									<button class="size-8 flex justify-center items-center bg-lime-500 hover:bg-lime-400 transition-colors duration-[250ms]" onclick={ () => {informations[i].accounts.splice(j, 1); !informations[i].accounts.length && informations.splice(i, 1);} }>
 										<svg class="w-auto h-1/2 fill-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="5 3 14 18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
@@ -213,46 +214,44 @@
 		</section>
 		<section class="flex flex-col flex-1 xl:gap-3">
 			<div class="flex flex-col flex-1 overflow-hidden border border-gray-400 rounded focus-within:border-black">
-				<select class="w-full h-12 tracking-wider text-black bg-gray-300 focus:outline-none xl:px-3" disabled={!informations.length && true} bind:value={inputSocialMedia}>
+				<select class="w-full h-12 tracking-wider text-black bg-slate-300 focus:outline-none xl:px-3" bind:value={inputSocialMedia}>
+					<option class="text-[red]">
+						{defaultSocialMedia}
+					</option>
 					{#each socialMediaEntries as socialMediaEntry}
 						<option>
 							{socialMediaEntry}
 						</option>
-						{:else}
-							<option>
-								No Account Added
-							</option>
 					{/each}
 				</select>
-				<textarea class="tracking-widest resize-none bg-gray-100 size-full focus:outline-none xl:p-3 placeholder:text-gray-500 focus:bg-white transition-colors duration-[250ms]" bind:value={inputText}></textarea>
+				<textarea class="tracking-widest resize-none bg-slate-100 size-full focus:outline-none xl:p-3 placeholder:text-slate-500 focus:bg-white transition-colors duration-[250ms]" bind:value={inputText} placeholder="Your message!"></textarea>
 			</div>
-			<button class="h-12 w-full flex justify-center items-center bg-zinc-500 hover:bg-zinc-600 transition-colors duration-[250ms] rounded" onclick={ convert }>
+			<button class="h-12 w-full flex justify-center items-center bg-slate-500 hover:bg-slate-600 transition-colors duration-[250ms] rounded" onclick={ convert }>
 				<svg class="w-auto h-1/2 fill-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="2 1 20 22"><path d="M0 0h24v24H0z" fill="none"></path><path d="M22 18v-2H8V4h2L7 1 4 4h2v2H2v2h4v8c0 1.1.9 2 2 2h8v2h-2l3 3 3-3h-2v-2h4zM10 8h6v6h2V8c0-1.1-.9-2-2-2h-6v2z"></path></svg>
 			</button>
 			<div class="flex flex-col flex-1 overflow-hidden border border-gray-400 rounded focus-within:border-black">
-				<p class="tracking-widest bg-gray-100 size-full xl:p-3">
+				<p class="tracking-widest bg-slate-100 size-full xl:p-3">
 					{@html outputText}
 				</p>
-				<select class="w-full h-12 tracking-wider text-black bg-gray-300 focus:outline-none xl:px-3" disabled={!informations.length && true} bind:value={outputSocialMedia}>
+				<select class="w-full h-12 tracking-wider text-black bg-slate-300 focus:outline-none xl:px-3" bind:value={outputSocialMedia}>
+					<option class="text-[red]">
+						{defaultSocialMedia}
+					</option>
 					{#each socialMediaEntries as socialMediaEntry}
 						<option>
 							{socialMediaEntry}
 						</option>
-						{:else}
-							<option>
-								No Account Added
-							</option>
 					{/each}
 				</select>
 			</div>
 		</section>
-		<section class="w-1/4">
-			<h1 class="tracking-wider text-white bg-orange-600 text-center leading-[3rem] border-b border-white rounded-t">
+		<section class="w-1/4 flex flex-col xl:gap-3">
+			<h1 class="tracking-wider text-white bg-orange-600 text-center leading-[3rem] rounded">
 				Accounts Mentioned
 			</h1>
-			<ul>
+			<ul class="flex flex-col xl:gap-3">
 				{#each nameEntries as nameEntry}
-					<li class="tracking-widest text-white bg-orange-400 border-b border-white xl:leading-8 xl:px-3">
+					<li class="tracking-widest text-white bg-orange-400 xl:leading-8 xl:px-3">
 						{nameEntry}
 					</li>
 				{/each}
