abf843ec3761d4e261f655526e3bcbb4bd2e8c38
Sina-Hosseini-GST <sina.hosseini.gst@gmail.com>
2024-11-25 14:09:16 +0330

    Fix sticky position, and fix the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const App = () => {
+
+  const myRef = useRef([]);
 
   let [informations, setInformations] = useState([]);
 
@@ -140,7 +142,7 @@ const App = () => {
                       const account = information.accounts[n];
                       if (information.name === outerInformation.name && account.socialMedia === outputSocialMedia) {
                         innerFlag = false;
-                        newOutputText += `<span class='text-[red] font-bold'>${account.userName}</span>` + space;
+                        newOutputText += `<span class='font-bold underline underline-offset-2'>${account.userName}</span>` + space;
                         break loop;
                       }
                     }
@@ -205,7 +207,7 @@ const App = () => {
     }
   };
 
-  const alertDuplicates = (informationIndex, accountIndex, event) => {
+  const alertSocialMediaDuplicates = (informationIndex, accountIndex, event) => {
     const socialMediaDuplicate = informations[informationIndex].accounts[accountIndex].socialMedia.trim();
 
     let flag = true;
@@ -214,7 +216,7 @@ const App = () => {
       const oldSocialMedia = accounts[i].socialMedia;
       if (oldSocialMedia === socialMediaDuplicate && i !== accountIndex) {
         alert(`${oldSocialMedia ? oldSocialMedia : 'empty'} account already added for ${informations[informationIndex].name}!`);
-        event.target.classList.remove('bg-lime-500');
+        event.target.classList.remove('bg-emerald-200');
         event.target.classList.add('bg-[red]');
         flag = false;
       }
@@ -224,13 +226,13 @@ const App = () => {
       // ul tag
       for (let i = 0; i < event.target.parentElement.parentElement.parentElement.children.length; i++) {
         event.target.parentElement.parentElement.parentElement.children[i].children[0].children[0].classList.remove('bg-[red]', 'bg-[orangered]');
-        event.target.parentElement.parentElement.parentElement.children[i].children[0].children[0].classList.add('bg-lime-500');
+        event.target.parentElement.parentElement.parentElement.children[i].children[0].children[0].classList.add('bg-emerald-200');
       }
     }
 
     if (!socialMediaDuplicate) {
       alert('empty!');
-      event.target.classList.remove('bg-lime-500');
+      event.target.classList.remove('bg-emerald-200');
       event.target.classList.add('bg-[orangered]');
     }
   };
@@ -254,13 +256,13 @@ const App = () => {
         <main className="flex flex-1 overflow-hidden xl:gap-3">
           <section className="flex flex-col w-1/4 xl:gap-3">
             <div className="flex tracking-widest xl:gap-3">
-              <button className="w-1/2 text-white bg-emerald-400 transition-colors duration-[250ms] hover:bg-emerald-500 text-center h-12 rounded flex justify-center xl:gap-2 items-center" onClick={exportInformations}>
+              <button className="w-1/2 bg-emerald-600 text-white transition-colors duration-[250ms] hover:bg-emerald-700 text-center h-12 rounded flex justify-center xl:gap-2 items-center" onClick={exportInformations}>
                 <svg className="w-auto h-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" color="" viewBox="3 3 18 18"><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
                 <span>
                   Export
                 </span>
               </button>
-              <label className="w-1/2 text-white bg-emerald-400 transition-colors duration-[250ms] hover:bg-emerald-500 text-center h-12 rounded flex justify-center xl:gap-2 items-center cursor-pointer">
+              <label className="w-1/2 bg-emerald-600 text-white transition-colors duration-[250ms] hover:bg-emerald-700 text-center h-12 rounded flex justify-center xl:gap-2 items-center cursor-pointer">
                 <input type="file" className="hidden" onChange={(event) => importInformations(event)} />
                 <svg className="w-auto h-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" color="" viewBox="3 3 18 18"><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>
                 <span>
@@ -268,19 +270,19 @@ const App = () => {
                 </span>
               </label>
             </div>
-            <h1 className="tracking-wider text-white bg-green-500 text-center leading-[3rem] rounded">
+            <h1 className="tracking-wider bg-white border text-black border-black text-center leading-[3rem] rounded">
               Accounts Added
             </h1>
             <div className="flex flex-col flex-1 overflow-auto tracking-widest xl:gap-3">
               {informations.length > 0 ? informations.map((information, i) => (
-                <div className="text-white" key={i}>
-                  <div className="flex overflow-hidden border-b border-white rounded-tl">
-                    <div className="flex items-center justify-center w-10 border-r border-white aspect-square shrink-0 bg-lime-400">
+                <div className="text-black" key={i}>
+                  <div className="sticky top-0 flex overflow-hidden border-b border-white rounded-tl">
+                    <div className="flex items-center justify-center w-10 font-bold text-white border-r border-white aspect-square shrink-0 bg-emerald-400">
                       <span className="-rotate-45">
                         {i + 1}
                       </span>
                     </div>
-                    <input placeholder="Give the account a name (e.g., Sina)" className="sticky top-0 z-10 w-full font-bold bg-lime-500 xl:leading-10 xl:px-3 focus:outline-none placeholder:text-gray-100" onChange={(event) => {
+                    <input placeholder="Give the account a name (e.g., Sina)" className="z-10 w-full font-bold bg-emerald-200 xl:leading-10 xl:px-3 focus:outline-none placeholder:text-gray-100" onChange={(event) => {
                       const newInformations = [...informations];
                       newInformations[i].name = event.target.value;
                       setInformations(newInformations);
@@ -291,20 +293,20 @@ const App = () => {
                     {information.accounts.map((account, j) => (
                       <li className="flex border-b border-white xl:leading-8" key={j}>
                         <div className="flex flex-1">
-                          <input placeholder="Social media (e.g., Instagram)" className="w-1/2 transition-colors duration-[250ms] border-r border-white bg-lime-500 xl:px-3 focus:outline-none placeholder:text-gray-100" onChange={(event) => {
+                          <input placeholder="Social media (e.g., Instagram)" className="w-1/2 transition-colors duration-[250ms] border-r border-white bg-emerald-200 xl:px-3 focus:outline-none placeholder:text-gray-100" onChange={(event) => {
                             const newInformations = [...informations];
                             newInformations[i].accounts[j].socialMedia = event.target.value;
                             setInformations(newInformations);
                           }
-                          } value={account.socialMedia} onBlur={(event) => alertDuplicates(i, j, event)} />
-                          <input placeholder="@username (e.g., @sinaGST)" className="w-1/2 border-r border-white bg-lime-500 xl:px-3 focus:outline-none placeholder:text-gray-100" onChange={(event) => {
+                          } value={account.socialMedia} onBlur={(event) => alertSocialMediaDuplicates(i, j, event)} />
+                          <input placeholder="@username (e.g., @sinaGST)" className="w-1/2 border-r border-white bg-emerald-200 xl:px-3 focus:outline-none placeholder:text-gray-100" onChange={(event) => {
                             const newInformations = [...informations];
                             newInformations[i].accounts[j].userName = event.target.value;
                             setInformations(newInformations);
                           }
                           } value={account.userName} />
                         </div>
-                        <button className="size-8 flex justify-center items-center bg-lime-500 hover:bg-lime-400 transition-colors duration-[250ms]" onClick={() => {
+                        <button className="size-8 flex justify-center items-center bg-emerald-300 hover:bg-emerald-400 transition-colors duration-[250ms]" onClick={() => {
                           if (confirm(`Delete ${information.name}'s ${account.socialMedia} account?`)) {
                             const newInformations = [...informations];
                             newInformations[i].accounts.splice(j, 1);
@@ -314,19 +316,19 @@ const App = () => {
                             setInformations(newInformations)
                           }
                         }}>
-                          <svg className="w-auto h-1/2 fill-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="5 3 14 18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+                          <svg className="w-auto h-1/2 fill-gray-900" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="5 3 14 18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
                         </button>
                       </li>
                     ))}
                   </ul>
-                  <button className="flex items-center justify-center w-full h-10 transition-colors duration-[250ms] bg-lime-500 hover:bg-lime-400" onClick={() => {
+                  <button className="flex items-center justify-center w-full h-10 transition-colors duration-[250ms] bg-emerald-300 hover:bg-emerald-400" onClick={() => {
                     if (confirm(`Delete all ${informations[i].name} accounts?`)) {
                       const newInformations = [...informations];
                       newInformations.splice(i, 1);
                       setInformations(newInformations);
                     }
                   }}>
-                    <svg className="w-auto h-1/2 fill-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="5 3 14 18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+                    <svg className="w-auto h-1/2 fill-gray-900" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="5 3 14 18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
                   </button>
                 </div>
               )) : (
@@ -336,7 +338,7 @@ const App = () => {
               )}
             </div>
             {informations.length > 0 && (
-              <button className="h-12 mt-auto w-full flex xl:gap-2 justify-center items-center bg-red-600 hover:bg-red-700 transition-colors duration-[250ms] rounded text-white tracking-widest" onClick={deleteEntries}>
+              <button className="h-12 mt-auto w-full flex xl:gap-2 justify-center items-center bg-emerald-600 text-white hover:bg-emerald-700 transition-colors duration-[250ms] rounded tracking-widest" onClick={deleteEntries}>
                 <svg className="w-auto h-1/2 fill-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="5 3 14 18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
                 <span>
                   Delete All Accounts
@@ -346,7 +348,7 @@ const App = () => {
           </section>
           <section className="flex flex-col flex-1 xl:gap-3">
             <div className="flex flex-col flex-1 overflow-hidden border border-gray-400 rounded focus-within:border-black">
-              <select className="w-full h-12 tracking-wider text-black bg-slate-300 focus:outline-none xl:px-3" onChange={(event) => {
+              <select className="w-full h-12 tracking-wider text-black bg-sky-300 focus:outline-none xl:px-3" onChange={(event) => {
                 setInputSocialMedia(event.target.value)
               }
               } value={inputSocialMedia}>
@@ -359,17 +361,17 @@ const App = () => {
                   </option>
                 ))}
               </select>
-              <textarea className="tracking-widest resize-none bg-slate-100 size-full focus:outline-none xl:p-3 placeholder:text-slate-500 focus:bg-white transition-colors duration-[250ms]" onChange={(event) => {
+              <textarea className="tracking-widest resize-none bg-sky-100 size-full focus:outline-none xl:p-3 placeholder:text-slate-500 focus:bg-white transition-colors duration-[250ms]" onChange={(event) => {
                 setInputText(event.target.value)
               }
               } value={inputText} placeholder="Your message!"></textarea>
             </div>
-            <button className="h-12 w-full flex justify-center items-center bg-slate-500 hover:bg-slate-600 transition-colors duration-[250ms] rounded" onClick={convert}>
+            <button className="h-12 w-full flex justify-center items-center bg-sky-500 hover:bg-sky-600 transition-colors duration-[250ms] rounded" onClick={convert}>
               <svg className="w-auto h-1/2 fill-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="" viewBox="2 1 20 22"><path d="M0 0h24v24H0z" fill="none"></path><path d="M22 18v-2H8V4h2L7 1 4 4h2v2H2v2h4v8c0 1.1.9 2 2 2h8v2h-2l3 3 3-3h-2v-2h4zM10 8h6v6h2V8c0-1.1-.9-2-2-2h-6v2z"></path></svg>
             </button>
             <div className="flex flex-col flex-1 overflow-hidden border border-gray-400 rounded focus-within:border-black">
-              <p className="tracking-widest bg-slate-100 size-full xl:p-3" dangerouslySetInnerHTML={{__html: outputText}} />
-              <select className="w-full h-12 tracking-wider text-black bg-slate-300 focus:outline-none xl:px-3" onChange={(event) => {
+              <p className="tracking-widest bg-sky-100 size-full xl:p-3" dangerouslySetInnerHTML={{__html: outputText}} />
+              <select className="w-full h-12 tracking-wider text-black bg-sky-300 focus:outline-none xl:px-3" onChange={(event) => {
                 setOutputSocialMedia(event.target.value)
               }} value={outputSocialMedia}>
                 <option className="text-[red]">
@@ -384,12 +386,12 @@ const App = () => {
             </div>
           </section>
           <section className="flex flex-col w-1/4 xl:gap-3">
-            <h1 className="tracking-wider text-white bg-orange-600 text-center leading-[3rem] rounded">
+            <h1 className="tracking-wider bg-white border text-black border-black text-center leading-[3rem] rounded">
               Accounts Mentioned
             </h1>
             <ul className="flex flex-col xl:gap-3">
               {nameEntries.map((nameEntry, index) => (
-                <li key={index} className="tracking-widest text-white bg-orange-400 xl:leading-8 xl:px-3">
+                <li key={index} className="tracking-widest text-white bg-green-800 rounded xl:leading-10 xl:px-3">
                   {nameEntry}
                 </li>
               ))}
